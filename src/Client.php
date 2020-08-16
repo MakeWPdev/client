39e099e1b94463e70684382721895379b76a885c
Hadie Danker <hadie87@gmail.com>
2020-08-17 01:27:08 +0700

    first commit

---
+++ src/Client.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Project :  elemenda.
+ * User: hadie MacBook
+ * Date: 17/08/20
+ * Time: 01.26
+ */
+
+namespace Makewpdev;
+
+/**
+ * MakeWP.dev Client
+ *
+ * This class is necessary to set project data
+ */
+class Client {
+
+}
